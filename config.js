45833f96e7023928c459db7447558445fff1c31b
Nico Ruiz <lifegamer2050@gmail.com>
2023-03-04 18:10:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,8 @@
 module.exports = {
-PTERODACTYL_URL: "https://panel.yourdomain.com", // Link to your Pterodactyl panel with protocol and without / at the end
-PTERODACTYL_TOKEN: "", // Pterodactyl API key created in admin area with read & write perms for Users and Servers
-
-SKIP_ADMIN_USERS: true, // Skip deleting of admin users in pterodactyl
-SKIP_SERVERS_IDS: [], // UUIDs of servers that you don't want to delete for eg, ["UID", "UID", "UID"]
-SKIP_USERS: [], // IDs of users which you don't want to delete and you don't want to delete servers owned by them too for eg, ["USERID", "USERID", "USERID"]
-FETCH_LIMIT: 5000 // How much users/servers do you want to fetch and delete at once (it will be used when making request to pterodactyl api)
-  
+PTERODACTYL_URL: "https://panel.yourdomain.com",
+PTERODACTYL_TOKEN: "",
+SKIP_ADMIN_USERS: true,
+SKIP_SERVERS_IDS: [],
+SKIP_USERS: [],
+FETCH_LIMIT: 
 };
